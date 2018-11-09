d77254aa49cbc55223ddd1dd7e265caca0bbb167
Noah Paessel <knowuh@gmail.com>
2018-11-09 17:41:24 -0500

    Style fixups for editor.

--- src/components/window-shade-form.tsx
+++ src/components/window-shade-form.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import * as css from "./window-shade-edit.sass";
+import * as css from "./window-shade-form.sass";
 
 import { IWindowShade, WindowShadeType, MediaType } from "../types";
 import { getContentConfiguration } from "../config/content-configurations";
