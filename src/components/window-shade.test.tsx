5cf14893bdd11fbf038b79a67f8cc85baa6f2119
Noah Paessel <knowuh@gmail.com>
2018-11-13 14:32:03 -0500

    Updated window-shade-test

--- src/components/window-shade.test.tsx
+++ src/components/window-shade.test.tsx
@@ -11,6 +11,6 @@ const windowShadeProps = {
 describe("WindowShade component", () => {
   it("renders Hello World", () => {
     const wrapper = shallow(<WindowShade authoredState={windowShadeProps}/>);
-    expect(wrapper.text()).toEqual("<WindowShade /><Markdown />");
+    expect(wrapper.text()).toEqual("<WindowShadeButton /><WindowShadeContent />");
   });
 });
