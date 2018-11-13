e8aa7a6ffc367d508038ea8dbe519312f0dea52f
dalove1025 <dalove1025@gmail.com>
2018-11-13 15:58:30 -0500

    Comment out console.log for travis/lint

--- src/components/window-shade-json.tsx
+++ src/components/window-shade-json.tsx
@@ -39,7 +39,7 @@ export default class WindowShadeJSON extends React.Component<IProps, IState> {
           this.props.onSave(newProps);
         }
         catch (error) {
-          console.log(error);
+          // console.log(error);
           this.setState({workingState: this.getAuthoredJson()});
         }
       }
