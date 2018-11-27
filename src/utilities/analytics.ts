4a5ff306748c63bc8588d53b660c2fad4ddbbe54
Noah Paessel <knowuh@gmail.com>
2018-11-27 16:00:44 -0500

    Some documentation

--- src/utilities/analytics.ts
+++ src/utilities/analytics.ts
@@ -1,4 +1,10 @@
 
+/**************************************************************
+ * This file exports a few interfaces and a single function
+ * `logAnalyticsEvent` which is a small wrapper around
+ * Google Analytics service custom event logging.
+ *************************************************************/
+
 import { TeacherTipType } from "../types";
 
 export enum AnalyiticsActionType {
@@ -14,20 +20,24 @@ export interface ILogEvent {
   readonly location?: string;
 }
 
+// See Google Documentation on event logging and `fieldsObject` here:
+// https://developers.google.com/analytics/devguides/collection/analyticsjs/events
 interface IGAData {
-  readonly hitType: "event";
   readonly eventCategory: string;
-  readonly eventAction?: string;
-  readonly eventLabel?: string;
+  readonly eventAction: string;
+  readonly eventLabel: string;
 }
 
 interface IAnalyticsService {
-  ga?: (send: "send", data: IGAData) => void;
+  ga?: (send: "send", type: "event", data: IGAData) => void;
 }
 
+// We create a service that looks like Google's `window.ga` interface
+// but mearly logs to the console.
+// TSLint doesn't like console messages.
 const mockGa = {
   // tslint:disable no-console
-  ga: (send: "send", data: IGAData) => {
+  ga: (send: "send", type: "event", data: IGAData) => {
     console.group("Mock analytics send payload:");
     console.debug(send);
     console.debug(JSON.stringify(data, null, 2));
@@ -40,17 +50,17 @@ export const logAnalyticsEvent = (event: ILogEvent) => {
   const windowWithPossibleGa = (window as IAnalyticsService);
 
   const payload: IGAData = {
-    hitType: "event",
     eventCategory: event.tipType,
     eventAction: event.eventAction,
     eventLabel: event.location ? event.location : event.tabName
   };
 
+  // Above all do no harm:
   try {
     if (windowWithPossibleGa.ga instanceof Function) {
-      windowWithPossibleGa.ga("send", payload);
+      windowWithPossibleGa.ga("send", "event", payload);
     } else {
-      mockGa.ga("send", payload);
+      mockGa.ga("send", "event", payload);
     }
   } catch (e) {
     // tslint:disable no-console
