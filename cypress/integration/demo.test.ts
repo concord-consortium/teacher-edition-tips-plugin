e033beab5bdf26ce509ed4dd9cacf46d8d239d3c
Noah Paessel <knowuh@gmail.com>
2019-11-01 16:33:43 -0400

    Attempt to address failing cypress test
    [#168702283]
    https://www.pivotaltracker.com/story/show/168702283

--- cypress/integration/demo.test.ts
+++ cypress/integration/demo.test.ts
@@ -1,3 +1,16 @@
+const resizeObserverLoopErrRe = /^ResizeObserver loop limit exceeded/
+
+// Handles rando `Uncaught Error: ResizeObserver loop limit exceeded`
+// which sometimes shows up in travis
+// see: https://github.com/quasarframework/quasar/issues/2233
+Cypress.on("uncaught:exception", (err) => {
+  if (resizeObserverLoopErrRe.test(err.message)) {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+  }
+});
+
 context("Test the demo app", () => {
   beforeEach(() => {
     cy.visit("/demo.html");
