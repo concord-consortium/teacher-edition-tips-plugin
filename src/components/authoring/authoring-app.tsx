6783b307a7a025e783042a9cf698214f97f28e18
Noah Paessel <knowuh@gmail.com>
2022-05-09 17:50:50 -0400

    181717687 - Update project dependencies
    - update eslint & rules
    
    Actually had to downgrade eslint a fair bit.
    
    [#181717687]
    https://www.pivotaltracker.com/n/projects/2441249/stories/181717687

--- src/components/authoring/authoring-app.tsx
+++ src/components/authoring/authoring-app.tsx
@@ -168,7 +168,7 @@ export default class AuthoringApp extends React.Component<IProps, IState> {
 
   private cloneState(newState: IAuthoredState) {
     const prevState = (this.state && this.state.authoredState) || this.props.initialAuthoredState;
-    return Object.assign({}, prevState, newState);
+    return { ...prevState, ...newState};
   }
 
   private setInitialState(): IAuthoredState {
