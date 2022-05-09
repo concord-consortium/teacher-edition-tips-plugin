926a867335456c7b994899eff46cc8c8c7a0dbcf
Noah Paessel <knowuh@gmail.com>
2022-05-09 16:58:10 -0400

    181717687 - Update project dependencies
    - Fix slightly broken webpack
    
    [#181717687]
    https://www.pivotaltracker.com/n/projects/2441249/stories/181717687

--- webpack.config.js
+++ webpack.config.js
@@ -4,91 +4,96 @@ const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const config = {
-  entry: {
-    demo: './src/demo.tsx',
-    authoring: './src/components/authoring/authoring-app',
-    plugin: './src/plugin.tsx'
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.ts(x)?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        use: ['@svgr/webpack'],
-      },
-      {
-        test: /\.(sa|sc|c)ss$/i,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
 
-            options: {
-              modules: {
-                mode: "local",
-                localIdentName: '[name]--[local]--TETipsPluginV1'
-              },
-              sourceMap: true,
-              importLoaders: 1,
-            }
-          },
-          'postcss-loader',
-          'sass-loader'
-        ]
-      },
-    ]
-  },
-  devServer: {
-    'static': {
-      directory: './dist'
+module.exports = (env, argv) => {
+  const devMode = argv.mode !== 'production';
+  return {
+    entry: {
+      demo: './src/demo.tsx',
+      authoring: './src/components/authoring/authoring-app',
+      plugin: './src/plugin.tsx'
     },
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    }
-  },
-  plugins: [
-    // new CopyPlugin({
-    //   patterns: [{
-    //     from: 'src/public/',
-    //     to: "dist/"
-    //   }],
-    // })
-    new HtmlWebpackPlugin({
+    devtool: devMode ? 'eval-cheap-module-source-map' : 'source-map',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          use: 'babel-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.ts(x)?$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.svg$/i,
+          issuer: /\.[jt]sx?$/,
+          use: ['@svgr/webpack'],
+        },
+        {
+          test: /\.(sa|sc|c)ss$/i,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+
+              options: {
+                modules: {
+                  mode: "local",
+                  localIdentName: '[name]--[local]--TETipsPluginV1'
+                },
+                sourceMap: true,
+                importLoaders: 1,
+              }
+            },
+            'postcss-loader',
+            'sass-loader'
+          ]
+        },
+      ]
+    },
+    devServer: {
+      'static': {
+        directory: './dist'
+      },
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/public/authoring.html',
+        filename: 'authoring.html',
+
+        chunks: ['authoring']
+      }),
+      new HtmlWebpackPlugin({
         template: './src/public/demo.html',
         filename: 'demo.html',
-    }),
-    // new CleanWebpackPlugin()
-  ],
-  externals: {
-    'react': 'React',
-    'react-dom': 'ReactDOM',
-    '@concord-consortium/lara-plugin-api': 'LARA.PluginAPI_V3'
-  },
-  resolve: {
-    extensions: [
-      '.tsx',
-      '.ts',
-      '.js'
+        chunks: ['demo']
+      })
+      // new CleanWebpackPlugin()
     ],
-    alias: {
-      'react-dom': '@hot-loader/react-dom'
+    externals: {
+      'react': 'React',
+      'react-dom': 'ReactDOM',
+      '@concord-consortium/lara-plugin-api': 'LARA.PluginAPI_V3'
+    },
+    resolve: {
+      extensions: [
+        '.tsx',
+        '.ts',
+        '.js'
+      ],
+      alias: {
+        'react-dom': '@hot-loader/react-dom'
+      }
     }
   }
 };
 
-module.exports = config;
