ac12e1874ea372353d6d505b36471974b46ab770
Noah Paessel <knowuh@gmail.com>
2022-06-02 17:00:47 -0400

    Workaround for jest not being able to load the Markdown component...
    Consider:
    Possibly using [ReactMardown](https://github.com/remarkjs/react-markdown)
    
    Also note we might get rid of markdown?

--- src/components/question-wrapper.tsx
+++ src/components/question-wrapper.tsx
@@ -62,6 +62,29 @@ export default class QuestionWrapper extends React.Component<IProps, IState> {
     this.logAction(AnalyticsActionType.loaded);
   }
 
+  // NP: 2022-06-02 - A bit hacky. Jest doesn't thing Markdown is a react
+  // component, so without this guard, jest fails tests. I can't figure out
+  // if there is something weird about how the Markdown component exports itself
+  // or why jest module loading wouldn't be the same as typescript module loading.
+  public renderMarkdown(visibleText: string, visibleTextClass: string) {
+    if (Markdown !== undefined) {
+      return (
+        <div className={visibleTextClass}>
+            <Markdown className={css.authorMarkdown}>
+              { visibleText }
+            </Markdown>
+        </div>
+      );
+    }
+    return (
+      <div className={visibleTextClass}>
+          <div className={css.authorMarkdown}>
+            { visibleText }
+          </div>
+      </div>
+    )
+  }
+
   public render() {
     const { activeTab } = this.state;
     const { authoredState, wrappedEmbeddableContext } = this.props;
@@ -130,12 +153,7 @@ export default class QuestionWrapper extends React.Component<IProps, IState> {
           { activeTab === "Distractors" && this.renderDistractorsOverlay() }
           { activeTab === "TeacherTip" && this.renderImageOverlay() }
           {
-            visibleText &&
-            <div className={visibleTextClass}>
-              <Markdown className={css.authorMarkdown}>
-                { visibleText }
-              </Markdown>
-            </div>
+            visibleText && this.renderMarkdown(visibleText, visibleTextClass)
           }
         </div>
       </div>
